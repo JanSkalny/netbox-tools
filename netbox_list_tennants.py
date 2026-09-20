2ccc2226b35151e6277acd0b5e39ce5775edf5ce
Jan Skalny <jan@skalny.sk>
2026-09-20 20:26:14 +0200

    list tennants

---
+++ netbox_list_tennants.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+
+import pynetbox
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-T', '--token', help='Netbox API Token (defaults to NETBOX_TOKEN env)', default=os.getenv('NETBOX_TOKEN'))
+parser.add_argument('-A', '--api-url', help='Netbox API URL (defaults to NETBOX_API_URL env)', default=os.getenv('NETBOX_API_URL'))
+args = parser.parse_args()
+
+nb = pynetbox.api(args.api_url, args.token)
+
+# fetch all tenants
+tenants = nb.tenancy.tenants.all()
+for tenant in tenants:
+  print(tenant.name)
